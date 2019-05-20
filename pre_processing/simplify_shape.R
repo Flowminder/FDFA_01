2fde8a85aebc2b3ce796d281a224e16b99bfc458
XVoll <X.Y.B.Vollenweider@gmail.com>
2019-05-20 21:51:39 +0200

    Save a simplified GoeJSON file for the shape for faster rendering (242->30.5 Mb)
    1) ALL_AdminUnits_final.shp (242 Mb) is simplified with the GRASS function -v.generalize.simplify-: ALL_AdminUnits_final_GRASS_simple.shp (162Mb)
    2) ALL_AdminUnits_final_GRASS_simple.shp is loaded into R and simplified with -rmapshaper::ms_simplify-: ALL_AdminUnits_final_light (30.5Mb)
    3) The number of unique ISO_NODE is compared between original and output file: 2198 in both cases
    4) ALL_AdminUnits_final_light is written to ALL_AdminUnits_final_light.geojson

---
+++ pre_processing/simplify_shape.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+library(dplyr)
+library(RPostgreSQL)
+library(leaflet)
+# set the directory ####
+setwd("C:/Users/Xavier Vollenweider/Dropbox/FDFA_01/data/")
+
+# load the data ####
+all_admin=rgdal::readOGR("spatial/All_AdminUnits_final/ALL_AdminUnits_final.shp")
+
+all_admin_GRASS_simple=rgdal::readOGR("spatial/All_AdminUnits_final_simplified/ALL_AdminUnits_final_GRASS_simple.shp") # data created with GRASS-> v.generalize.simplify
+
+# simplify shape for faster map rendering ####
+all_admin_s=rmapshaper::ms_simplify(all_admin_GRASS_simple,
+                                    keep=0.1)
+
+print(object.size(all_admin),units = "auto") # from 243 Mb to 
+print(object.size(all_admin_s),units = "auto") # 30.5 Mb
+
+# check no ISO_NODE has been lost ####
+length(unique(all_admin$ISO_NODE))
+length(unique(all_admin_s$ISO_NODE))
+
+# write as GeoJSON ####
+rgdal::ogrDrivers()
+rgdal::writeOGR(all_admin_s,
+                "spatial/All_AdminUnits_final_simplified/ALL_AdminUnits_final_light.geojson",
+                driver = "GeoJSON",
+                layer=1)
