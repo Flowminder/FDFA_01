4aa017837b7ca5804c437e49244ba43fa25019b9
XVoll <X.Y.B.Vollenweider@gmail.com>
2019-05-21 19:04:42 +0200

    added sum of in and out migration in the Net table

--- pre_processing/database_sqlite.R
+++ pre_processing/database_sqlite.R
@@ -7,6 +7,8 @@ table_gender_mig=read.csv("table/IntMigrEst.csv")
 
 table_nick_mig=read.csv("table/combined_df_20180122.csv")
 
+all_admin_light=rgdal::readOGR("spatial/All_AdminUnits_final_simplified/all_admin_simplified.geojson")
+
 # Emigration movements per admin unit ####
 EM_data=table_gender_mig%>%
   mutate(ISO_NODEJ=paste(ISOI,as.integer(NODEJ),sep="_"),
@@ -45,34 +47,40 @@ Net_MIG=IM_data%>%
                      "females_EM"="females",
                      "males_EM"="males"),
             by=c("ISO_NODE"))%>%
-  mutate(net_IM=total-total_EM,
+  mutate(EM_IM=total+total_EM,
+         EM_IM_females=females+females_EM,
+         EM_IM_males=males+males_EM,
+         net_IM=total-total_EM,
          net_EM=total_EM-total,
-         net_IM_female=females-females_EM,
-         net_EM_female=females_EM-females,
-         net_IM_male=males-males_EM,
-         net_EM_male=males_EM-males)%>%
-  select(ISO_NODE,net_IM,net_EM,net_IM,net_IM_female,net_EM_female,net_IM_male,net_EM_male)
+         net_IM_females=females-females_EM,
+         net_EM_females=females_EM-females,
+         net_IM_males=males-males_EM,
+         net_EM_males=males_EM-males)%>%
+  select(ISO_NODE,net_IM,net_EM,net_IM,net_IM_females,net_EM_females,net_IM_males,net_EM_males,EM_IM,EM_IM_females,EM_IM_males)
 
 # add the tables in a SQLITE database ####
 mig_db <- src_sqlite("table/mig_db.sqlite3",
-                    create = T)
+                     create = T)
 copy_to(mig_db,
         EM_data,
         name="EM",
         temporary = FALSE,
-        indexes = list("ISO_NODE"))
+        indexes = list("ISO_NODE"),
+        overwrite = T)
 
 copy_to(mig_db,
         IM_data,
         name="IM",
         temporary = FALSE,
-        indexes = list("ISO_NODE"))
+        indexes = list("ISO_NODE"),
+        overwrite = T)
 
 copy_to(mig_db,
         Net_MIG,
         name = "Net",
         temporary = FALSE,
-        indexes = list("ISO_NODE"))
+        indexes = list("ISO_NODE"),
+        overwrite = T)
 
 # store table_gender_mig in the database ####
 table_gender_mig_s=table_gender_mig%>%
@@ -101,3 +109,15 @@ copy_to(mig_db,
         indexes = list("CONTI","ISOI","ISO_NODEI","CONTJ","ISOJ","ISO_NODEJ"),
         overwrite = T)
 
+# store data from the SpatialPolygonDataFrame all_admin_light in the database ####
+all_admin_light_db=all_admin_light
+all_admin_light_db@data=all_admin_light_db@data%>%
+  mutate(ISO_NODE=as.character(ISO_NODE))%>%
+  select(CONT,ISO,ISO_NODE,TotPop,UrbanPop,UrbanProp)
+
+copy_to(mig_db,
+        all_admin_light_db@data,
+        name="admin",
+        temporary = FALSE,
+        indexes = list("CONT","ISO","ISO_NODE"),
+        overwrite = T)
