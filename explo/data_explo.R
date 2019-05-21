39476e2d2a6ab4e664d0f838436e2975976aaac7
XVoll <X.Y.B.Vollenweider@gmail.com>
2019-05-21 14:54:51 +0200

    explore gender disaggregregated data

--- explo/data_explo.R
+++ explo/data_explo.R
@@ -10,12 +10,15 @@ library(leaflet)
 setwd("C:/Users/Xavier Vollenweider/Dropbox/FDFA_01/data/")
 
 # load the data ####
-table_mig=read.csv("table/combined_df_20180122.csv")
+table_nick_mig=read.csv("table/combined_df_20180122.csv")
 
-all_admin_light=rgdal::readOGR("spatial/All_AdminUnits_final_simplified/ALL_AdminUnits_final_light.geojson")
+table_gender_mig=read.csv("table/IntMigrEst.csv")
 
-# Select origin data Dar es Salaam ####
-dar_data=table_mig%>%
+all_admin_light=rgdal::readOGR("spatial/All_AdminUnits_final_simplified/all_admin_simplified.geojson")
+
+# Nick's model of internal migration ####
+#  Select destination data Dar es Salaam 
+dar_data=table_nick_mig%>%
   filter(ISOJ=="TZA" & NODEJ==7,
          is.na(movein)==F,
          pred>0)%>%
@@ -24,7 +27,7 @@ dar_data=table_mig%>%
   arrange(ISO_NODEI)%>%
   select(pred,ISO_NODEI,ISO_NODEJ)
 
-# show on leaflet ####
+# show on leaflet 
 data_to_plot=all_admin_light
 
 data_to_plot@data=data_to_plot@data%>%
@@ -43,5 +46,41 @@ leaflet(data_to_plot)%>%
   addLegend("bottomright",
             colors = ~colorQuantile("Greens", pred)(pred), 
             labels = ~pred,
+            opacity = 1)
+
+# Gender disaggregated model of internal migration ####
+names(table_gender_mig)
+dim(table_gender_mig)
+head(table_gender_mig)
+table(table_gender_mig$NODEI[table_gender_mig$ISOI=="ARG"])
+table(table_gender_mig$NODEJ[table_gender_mig$ISOI=="ARG"])
+
+#  Select destination data Buenos Aires 
+BA_data=table_gender_mig%>%
+  filter(ISOI=="ARG" & NODEJ==2)%>%
+  mutate(ISO_NODEJ=paste(ISOI,as.integer(NODEJ),sep="_"),
+         ISO_NODEI=paste(ISOI,as.integer(NODEI),sep="_"))%>%
+  arrange(ISO_NODEI)%>%
+  select(MIGIJ_F_est,MIGIJ_M_est,ISO_NODEI,ISO_NODEJ)
+
+head(BA_data)
+# show on leaflet 
+data_to_plot=all_admin_light
+
+data_to_plot@data=data_to_plot@data%>%
+  mutate(ISO_NODE=as.character(ISO_NODE))%>%
+  left_join(BA_data,
+            by=c("ISO_NODE"="ISO_NODEI"))
+
+leaflet(data_to_plot)%>%
+  addPolygons(weight=1,
+              color = "#444444",
+              smoothFactor = 1,
+              fillOpacity = 1,
+              fillColor = ~colorQuantile("Greens", MIGIJ_F_est)(MIGIJ_F_est),
+              popup = ~MIGIJ_F_est)%>%
+  addLegend("bottomright",
+            colors = ~colorQuantile("Greens", MIGIJ_F_est)(MIGIJ_F_est), 
+            labels = ~MIGIJ_F_est,
             opacity = 1)
 
