e9b9c104dea429fb3cd2bb1a4b1ce48746e5c76c
XVoll <X.Y.B.Vollenweider@gmail.com>
2019-05-21 15:17:49 +0200

    First stab at the UI
    Only 1 tab showing the map.
    
    Display options (need to shop down, some illogism):
    * Movement (dropdown): total admin unit outflow, inflow, netflow and total flow
    * Gender (dropdown): total, male, female, female as % of total, male as % of total
    * Destination (checkbox):check box, if checked shows origin/dest for a selected admin unit
    * Direction (dropdown): if Destination is checked, select origin, destination, net departures, net arrivals
    * Metric (dropdown): number of people, proprtion of destination, proportion of origin

---
+++ app/ui.R
@@ -0,0 +1,61 @@
+###################
+# ui #
+###################
+ui <- shinyUI(navbarPage("Gender-disaggregated Migration Movements",
+                         
+                         tabPanel("Map",
+                                  fluidPage(
+                                    tags$head(tags$style(
+                                      HTML('
+                                           #PANEL {background-color: rgba(190,190,190,0.5);}')
+                                    )),
+                                    column(12,
+                                           leafletOutput("map",width="100%",height="1200px"),
+                                           
+                                           absolutePanel(top = 50, right = 15,
+                                                         draggable = TRUE,
+                                                         class = "panel panel-default",
+                                                         id="PANEL",
+                                                         
+                                                         selectInput("direction", 
+                                                                     label = "Movements",
+                                                                     choices = c("Total migration"="total_migration",
+                                                                                 "Internal Emigration"="emigration",
+                                                                                 "Internal Immigration"="immigration",
+                                                                                 "Net immigration"="net_immigration"),
+                                                                     selected = "PERF"),
+                                                         
+                                                         selectInput("gender", 
+                                                                     label = "Gender",
+                                                                     choices = c("Total (females and males)"="total",
+                                                                                 "Females"="females",
+                                                                                 "Males"="males",
+                                                                                 "Females as percent of total"="females_perc",
+                                                                                 "Males as percent of total"="males_perc"),
+                                                                     selected = "total")
+                                                         
+                                                         checkboxInput("destination",
+                                                                       "Destination",
+                                                                       value = F),
+                                                         
+                                                         selectInput("direction", 
+                                                                     label = "Direction",
+                                                                     choices = c("Origin"="origin",
+                                                                                 "Destination"="destination",
+                                                                                 "Net departure"="net_departure",
+                                                                                 "Net arrival"="net_arrival"),
+                                                                     selected = "origin"),
+                                                         
+                                                         selectInput("metric", 
+                                                                     label="Metric",
+                                                                     choices = c("Number of people"="number",
+                                                                                 "Proportion of destination"="prop_destination",
+                                                                                 "Proportion of origin"="prop_origin"),
+                                                                     selected = "number"))
+                                           
+                                    )
+                                  )
+                         )
+)
+)
+
