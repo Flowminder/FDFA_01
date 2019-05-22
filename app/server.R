a5605342e2839a9fd9072b836245c61d9522308b
XVoll <X.Y.B.Vollenweider@gmail.com>
2019-05-22 12:20:24 +0200

    Map click event and labels
    Fixed map_shap_click event (bug caused by popup)
    Added LayerIds for each poly for faster identification: no need to use coordinates
    
    Labels: placeholder set, need to add human readable country and admin  name

--- app/server.R
+++ app/server.R
@@ -29,27 +29,82 @@ server <- shinyServer(function(input, output, session) {
     return(data_to_map)
   })
   
+  # Map labels ####
+  labels=reactive({ 
+    
+    data_to_map_collected=data_to_map()
+    
+    field_to_map=input$gender
+    
+    labels=sprintf( # label
+      paste("<strong>%s</strong><br/>%s", field_to_map),
+      
+      unlist(data_to_map_collected@data[,"CONT"]),
+      
+      formatC(unlist(data_to_map_collected@data[,field_to_map]),
+              format="f", 
+              digits=0, 
+              big.mark="'")
+    ) %>% lapply(htmltools::HTML)
+    
+    return(labels)
+    
+  })
+  
   # Map ####
   observe({
     data_to_map_collected=data_to_map()
     
     field_to_map=input$gender
-    # leaflet(data_to_map)%>%
-    #   addPolygons(weight=1,
-    #               color = "#444444",
-    #               smoothFactor = 1,
-    #               fillOpacity = 1,
-    #               fillColor = ~colorQuantile("Greens", get("total"))(get("total")),
-    #               popup = ~get("total"))
     
-    p<-leafletProxy("map",
-                    data=data_to_map_collected)%>%
-      addPolygons(weight=1,
+    labels=labels()
+    
+    p<-leafletProxy("map")%>%
+      addPolygons(data=data_to_map_collected,
+                  weight=1,
                   color = "#444444",
-                  smoothFactor = 1,
                   fillOpacity = 1,
                   fillColor = ~colorQuantile("Greens", get(field_to_map))(get(field_to_map)),
-                  popup = ~get(field_to_map))
+                  layerId = 1:dim(data_to_map_collected)[1],
+                  label = labels,
+                  labelOptions = labelOptions(
+                    style = list("font-weight" = "normal", padding = "3px 8px"),
+                    textsize = "15px",
+                    direction = "auto"))
+    
     return(p)
+  })
+  
+  # Admin_clic ####
+  # observeEvent(input$map_click,{
+  #   print("hh")
+  #   event <- input$map_click
+  #   print(event)
+  # })
+  
+  observeEvent(input$map_shape_click,{
+    event <- input$map_shape_click
+    print(event)
+    if(is.null(event)){
+      event<-data.frame("lng"=-58.50,
+                        "lat"=-34.60)
+    }
+    
+    coord_select<-data.frame("lon"=event$lng,
+                             "lat"=event$lat)
+    
+    coordinates(coord_select)<-c("lon","lat")
+    proj4string(coord_select) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+    
+    
+    selected_adm<-sp::over(coord_select,
+                           admin_poly)
+    
+    selected_adm<-c(as.character(selected_adm$ISO_NODE))
+    
+    
+    print(selected_adm)
+    
+    #return(selected_adm)
   })
 })
