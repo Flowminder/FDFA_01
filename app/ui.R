da44c100bdfc81ee7c687cb96700f11c9023b911
XVoll <X.Y.B.Vollenweider@gmail.com>
2019-05-23 13:42:42 +0200

    Unpack UI for easier edit
    1) components
    2) structure

--- app/ui.R
+++ app/ui.R
@@ -1,52 +1,61 @@
 ###################
 # ui #
 ###################
+
+# UI components ####
+leaflet_map=leafletOutput("map",width="100%",height="1200px")
+
+dropdown_movement=selectInput("movement",
+                              label = "Movements",
+                              c("Total movements at admin unit"="movements",
+                                "Origin-Destination from and to admin unit"="origin_destination"),
+                              selected = "movements")
+
+dropdown_direction=selectInput("direction", 
+                               label = "Direction",
+                               choices = c("Emigration"="emigration",
+                                           "Immigration"="immigration",
+                                           "Net immigration"="net_immigration",
+                                           "Net Emigration"="net_emigration",
+                                           "Total migration (in and out)"="total_migration"),
+                               selected = "emigration")
+
+dropdown_gender=selectInput("gender", 
+                            label = "Gender",
+                            choices = c("Total (females and males)"="total",
+                                        "Females"="females",
+                                        "Males"="males",
+                                        "Females as percent of total"="females_perc",
+                                        "Males as percent of total"="males_perc"),
+                            selected = "total")
+
+dropdown_basemap=selectInput("base_map", 
+                             label = "Base map",
+                             choices = c("Open street map"="OSM",
+                                         "Population density"="pop_dens"),
+                             selected = "OSM")
+
+
+widget_panel=absolutePanel(top = 50, left = 15,
+                           draggable = TRUE,
+                           class = "panel panel-default",
+                           id="PANEL",
+                           dropdown_movement,
+                           dropdown_direction,
+                           dropdown_gender,
+                           dropdown_basemap)
+
+panel_style=tags$head(tags$style(HTML('#PANEL {background-color: rgba(190,190,190,0.5);}')))
+
+# UI structure ####
 ui <- shinyUI(navbarPage("Gender-disaggregated Migration Movements",
                          
                          tabPanel("Map",
                                   fluidPage(
-                                    tags$head(tags$style(
-                                      HTML('
-                                           #PANEL {background-color: rgba(190,190,190,0.5);}')
-                                    )),
+                                    panel_style,
                                     column(12,
-                                           leafletOutput("map",width="100%",height="1200px"),
-                                           
-                                           absolutePanel(top = 50, left = 15,
-                                                         draggable = TRUE,
-                                                         class = "panel panel-default",
-                                                         id="PANEL",
-                                                         
-                                                         selectInput("movement",
-                                                                     label = "Movements",
-                                                                       c("Total movements at admin unit"="movements",
-                                                                         "Origin-Destination from and to admin unit"="origin_destination"),
-                                                                     selected = "movements"),
-                                                         
-                                                         selectInput("direction", 
-                                                                     label = "Direction",
-                                                                     choices = c("Emigration"="emigration",
-                                                                                 "Immigration"="immigration",
-                                                                                 "Net immigration"="net_immigration",
-                                                                                 "Net Emigration"="net_emigration",
-                                                                                 "Total migration (in and out)"="total_migration"),
-                                                                     selected = "emigration"),
-
-                                                         selectInput("gender", 
-                                                                     label = "Gender",
-                                                                     choices = c("Total (females and males)"="total",
-                                                                                 "Females"="females",
-                                                                                 "Males"="males",
-                                                                                 "Females as percent of total"="females_perc",
-                                                                                 "Males as percent of total"="males_perc"),
-                                                                     selected = "total"),
-                                                         
-                                                         selectInput("base_map", 
-                                                                     label = "Base map",
-                                                                     choices = c("Open street map"="OSM",
-                                                                                 "Population density"="pop_dens"),
-                                                                     selected = "OSM")
-                                           )
+                                           leaflet_map,
+                                           widget_panel
                                            
                                     )
                                   )
